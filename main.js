86808223d7d509599e18bd39390b7ce4923057f1
wadejs <379664673@qq.com>
2019-03-06 11:40:51 +0800

    change template

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/index.styl'
+import './assets/css/animate.css'
 
 Vue.config.productionTip = false
 
