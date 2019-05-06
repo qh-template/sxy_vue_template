d4d8b348b72ee95e9362ee68dc53f7598bab9096
qh-template <40918207+qh-template@users.noreply.github.com>
2019-05-06 18:24:25 +0800

    change
    change

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, `../../../${folder}`),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
